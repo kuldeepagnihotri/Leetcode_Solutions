f8384f452701da25d9ad4a8c82d59c02cf60cd61
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-06-22 13:56:30 +0530

    Time: 0 ms (100%), Space: 10.7 MB (92.89%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxWindow = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            freq[s[right] - 'A']++;
+
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            int windowLength = right - left + 1;
+
+            if (windowLength - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            windowLength = right - left + 1;
+            maxWindow = max(maxWindow, windowLength);
+        }
+
+        return maxWindow;
+    }
+};
