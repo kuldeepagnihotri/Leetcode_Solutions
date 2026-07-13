dd654d5672ebe5cf392f8d8c953b15a22001e8ec
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-07-13 14:55:03 +0530

    Time: 2 ms (24.72%), Space: 23.7 MB (72.91%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -10,25 +10,19 @@
  * };
  */
 class Solution {
-    private:
-    int maxDia;
-    int dfs(TreeNode*node){
-        if(node == nullptr)//base case
-        return 0;
-//recursively find the longest path both left and right
-        int leftPath = dfs(node->left);
-        int rightPath = dfs(node->right);
-//update the diameter 
-        maxDia=std :: max(maxDia, leftPath+rightPath);
-//return the longest one between the left and right +1
-        return std :: max(leftPath,rightPath) + 1;
-    }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        maxDia = 0;
-        dfs(root);//first call of dfs
-        return maxDia;
-
+        int diameter = 0;
+        height(root,diameter);
+        return diameter;
         
+    }
+    private:
+    int height(TreeNode* root,int & diameter){
+        if(root==NULL) return 0;
+        int lh=height(root->left,diameter);
+        int rh=height(root->right,diameter);
+        diameter=max(diameter,lh+rh);
+        return  1+ max(lh,rh);
     }
 };
