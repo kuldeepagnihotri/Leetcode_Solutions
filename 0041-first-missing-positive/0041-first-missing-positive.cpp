ed7c225f5e8751e223c59d8222d61aa8bc5123f4
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-08 10:23:51 +0530

    Time: 19 ms (20.33%), Space: 54.9 MB (86.4%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public://brute force 
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=1;
+        for(int x:nums){
+            if(x==ans)ans++;
+        }
+        return ans;
+    }
+};
