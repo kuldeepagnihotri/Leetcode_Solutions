d7cfdb5553c3e9a2cc2bf7a16d771a7e2f368bd7
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-08 10:24:54 +0530

    Time: 0 ms (100%), Space: 54.9 MB (86.4%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,11 +1,23 @@
 class Solution {
-public://brute force 
+public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans=1;
-        for(int x:nums){
-            if(x==ans)ans++;
+        int n = nums.size();
+
+        // Put every number x at index x-1
+        for(int i = 0; i < n; i++) {
+            while(nums[i] >= 1 && nums[i] <= n &&
+                  nums[nums[i] - 1] != nums[i]) {
+                
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
-        return ans;
+
+        // Find first position where number is incorrect
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
     }
 };
