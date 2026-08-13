a4f274ad19c1e0c3d9d4602299cfaf52bf93a134
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-13 10:21:55 +0530

    Time: 337 ms (5.64%), Space: 131.3 MB (5.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-     vector<int> dict(256,-1);
-     int maxLen=0;
-     int start=-1;
-          for(int i=0;i<s.size();i++){
-            if(dict[s[i]]>start)
-            start=dict[s[i]];
-            dict[s[i]]=i;
-            maxLen=max(maxLen,i-start);
-          }
-        return maxLen;
+      int ans =0;
+      int i =0;
+      int j =0;
+      set<char> st;
+      while(j<s.length()){
+        // if no dupliactes found then we will add that value in answer
+       if(st.find(s[j])==st.end()){
+        st.insert(s[j]);
+        j++;
+        ans=max(ans,j-i);
+       }
+       else{
+        // if dupliactes found then shrink the window from the left and again update answer
+        st.erase(s[i]);
+        i++;
+       }
+      }  
+      return ans ;
     }
 };
