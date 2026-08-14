c8788091f69925317e9ab39cf6649ba0878a6fe7
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-14 14:54:25 +0530

    Time: 15 ms (35.66%), Space: 14.6 MB (74.57%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-       vector<int> freq(26,0);
+        // brute force
+        int n = s.length();
+        for(int i =0;i<n;i++){
+            bool unique=true;
         
-        for(char ch:s){
-            freq[ch-'a']++;
-
+        for(int j =0;j<n;j++){
+            if(i!=j && s[i]==s[j]){
+                unique=false;
+                break;
+            }
         }
-       for(int i =0;i<s.size();i++){
-        if(freq[s[i]-'a']==1)
-        return i;
-       }
-        return -1;
+        if(unique) return i;
+        
     }
-   
+    return -1;
+    }
 };
