337764d88a336d4efc220a3d11c04de2db833c07
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-21 02:04:56 +0530

    Time: 2 ms (92.97%), Space: 13.5 MB (50.1%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        j=0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+              nums[i],nums[j]= nums[j],nums[i]
+              j+=1
+                
+        
