f264e1d54bc182c16ee6f76969355a863bf0a3d6
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-21 23:35:24 +0530

    Time: 0 ms (100%), Space: 12.4 MB (69.62%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        ans=""
+        for i in range(len(strs[0])):
+           ch=strs[0][i]
+           for j in range( 1,len(strs)):
+               if i >= len(strs[j]) or strs[j][i]!=ch:
+                    return ans
+           ans+=ch
+
+        return ans               
+        
