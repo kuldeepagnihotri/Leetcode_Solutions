497342a49f5ecc8ac4c5132221198bdc52721f55
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-08-30 19:35:27 +0530

    Time: 1050 ms (5.05%), Space: 60.7 MB (5.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.size(), n2 = s2.size();
-        if (n2 < n1) return false;
+       int n=s1.length();
+       int m=s2.length();
+       if(n>m ) return false;
 
-        vector<int> c1(26, 0), c2(26, 0);
-
-        for (int i = 0; i < n1; i++) {
-            c1[s1[i] - 'a']++;
-            c2[s2[i] - 'a']++;
-        }
-
-        if (c1 == c2) return true;
-
-        for (int i = n1; i < n2; i++) {
-            c2[s2[i] - 'a']++;
-            c2[s2[i - n1] - 'a']--;
-
-            if (c1 == c2) return true;
-        }
-
-        return false;
+       sort(s1.begin(),s1.end());
+for(int i =0;i<=m-n;i++){
+    string substring=s2.substr(i,n);
+    sort(substring.begin(),substring.end());
+    if(s1==substring) return true;
+}
+return false;
     }
 };
