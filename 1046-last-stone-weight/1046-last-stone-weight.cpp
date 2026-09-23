f5e3f953f90991245b4500d5d8608b7ce753ded3
KULDEEP AGNIHOTRI <kuldeepagnihotri18@gmail.com>
2026-09-23 23:34:01 +0530

    Time: 0 ms (100%), Space: 10 MB (12.44%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(),stones.end());
+        while(pq.size()>1){
+            int a=pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            if(a==b) continue;
+            pq.push(a-b);
+        }
+        if(pq.size()==0) return 0;
+        return pq.top();
+    }
+};
